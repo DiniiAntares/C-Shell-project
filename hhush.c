8932862e25f5c64f925277b26deea72bb4a50a66
Simon Franz <simon.franz@hhu.de>
2014-08-09 23:35:02 +0200

    I know what I did!

--- hhush.c
+++ hhush.c
@@ -7,7 +7,7 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 
 //Allowed includes
 // #include < stdlib>
@@ -31,66 +31,147 @@
  */
 int exit();
 int date();
-String cd(String /*insert lineString*/);
-String echo(String /*insert lineString*/);
-String[] history();
+char cd[](char /*insert lineString*/);
+char echo[](char /*insert lineString*/);
+char history[]();
 int ls();
-
+int time();
 /*
  * Prints the current date+time.
  */
 int date(){
-    
+    char day[];
+    int day;
+    char month[];
+    int year;
 }
-
+/**
+ * help method for date() and print's current time.
+ */
+char time[](){
+    
+    time_t currentTime = time();
+    printf("%s", asctime(localtime(&result)));
+    return currentTime;
+}
 /**
  * Closes the program.
  */
 int exit(){
-    
-    return 0;
+    main.active = false; //Stops the loop.
+    return EXIT_SUCCES;
 }
 
 /**
  * Changes dir.
  */
-String cd(String /*insert lineString*/){
-    
+char cd[](char []/*insert lineString*/){
+    switch (){
 }
 
 /**
  * Shows the history.
  */
-String[] history(){
-    String i[];
+char history[](int lines){
+    char list[];
     return i;
 }
 
 /**
  * Echos the written String.
  */
-String echo(String /*insert lineString*/){
+char echo[](char whatToEcho[]){
+    printf("%s", whatToEcho);
     
 }
 
-/*
- * Main
+/**
+ * Prints the current directions content.
  */
-int main(){
-  
-  for (_Bool active=true; active==true;){ //loop to run the projec
-      
-      
-      if (){}
-      else if (){exit();}//do exit
-      else if (){dater();}//do date
-      else if (){cd();}//do cd
-      else if (){echo();}//do echo
-      else if (){history();}//do history
-      else if (){ls();}//do ls
-      else if (){grep();}//do grep
-      else{ println("Command not found."); }//Print "Command not found."
-  }
+char ls[](){
+    printf();
 }
 
-//fgets(buf, 255, stdin);
+
+/*
+ *Filters the command
+ */
+char commandReader[](char input[]){ //Input is given by main
+    
+    if (strlen(input)<259) {
+        int k = 0;
+        for (int i=0; i<259 ;i++){ //Condition to look out for WS(Whitespaces)
+            if (input[i] != " " || inpur[i] != "\t"){
+                if ( input[i+1] = " " || input[i+1] = "\t"){
+                    
+                    char usable[i+1];       //Create a new String/CharArray to contain the non whitespace parts
+                    for (int j=k;j<i+1;j++){ //j=k because of the possible WhiteSpaces at the beginning of the String
+                        usable[j-k] = input[j];
+                    }
+                    return usable[];
+                }
+            }
+            else k++; //Counts the WS before the command
+        }
+    }
+    else{ 
+        printf("Your input is wrong.");//Print default message for unusable input
+        return EXIT_SUCCES;
+        
+    }
+}
+
+/*
+ * Filters the content besides the command.
+ */
+char contentReader[](char fullInput[], int sizeOfCommand){
+    char content;
+    for (int i = sizeOfCommand-1 ; i < sizeof(fullInput[])+1; i++){
+        content[j] = fullInput[i];
+    }
+    return content[];
+}
+
+/*
+ * Main method to run the shell.
+ */
+int main(){
+    for (_Bool active=true; active==true;){ //loop to run the projec
+        println(/*directory*/  + " $ ",%s);   //Print current directory
+        char input[258];
+        fgets(input, 258, stdin);
+        
+        {Insert code}//add new line to history
+        
+        char input[258];
+        fgets(input, 258, stdin);
+        char command[] = commandReader(input[]); //read out the command.
+        char content[] = contentReader(input[], sizeof(command)); //read out the rest
+        
+        do{
+            if (command[] == exit){
+                exit();
+            }
+            else if (command[] == date){ 
+                date();
+            }
+            else if (command[] == cd){ 
+                cd(content[]);
+            }
+            else if (command[] == echo){
+                echo(content[]);
+            }
+            else if (command[] == history){
+                history(content[]);
+            }
+            else if (command[] == ls){
+                ls();
+            }
+            
+        }
+
+    }
+return EXIT_SUCCES;
+}
+
+//Bspl: fgets([var], [length], stdin);
